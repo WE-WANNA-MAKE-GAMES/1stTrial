e7a520bba83ceb2e7cc7a59503a65af77f154813
Photeinos-Kochlias <mogurabu.j@gmail.com>
2026-08-09 16:10:36 +0900

    マクロファージの弾の画面外判定を修正

--- Assets/scripts/Bullet/MacrophageBullet.cs
+++ Assets/scripts/Bullet/MacrophageBullet.cs
@@ -1,17 +1,29 @@
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class MacrophageBullet : MonoBehaviour
 {
     public float speed = -15f;   // Speed at which the bullet moves
+    [SerializeField] float destroyDistance = 15f;
     void Update()
     {
         transform.position += Vector3.right * speed * Time.deltaTime;   // Move the bullet to the right at the specified speed
+
+        // Destroy bullets when they become invisible
+        if (transform.position.x < Camera.main.transform.position.x - destroyDistance)
+        {
+            Destroy(gameObject);
+        }
     }
+    /* //!なぜか消えない
     // Destroy the bullet when it goes off-screen to prevent memory leaks
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
     }
+    */
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("PlayerDamageReceiver") || other.CompareTag("Player"))  // Check if the bullet collides with the player
@@ -23,7 +35,7 @@ public class MacrophageBullet : MonoBehaviour
             }
             Destroy(gameObject);
         }
-        else if(other.CompareTag("Bullet"))  // Check if the bullet collides with another bullet
+        else if (other.CompareTag("Bullet"))  // Check if the bullet collides with another bullet
         {
             Destroy(other.gameObject); // Destroy the bullet
             Destroy(gameObject); // Destroy this bullet as well
