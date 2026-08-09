dc2b77197e70cccdafd227d34984eafec4513797
Photeinos-Kochlias <mogurabu.j@gmail.com>
2026-08-09 16:53:56 +0900

    change directory structure

---
+++ Assets/scripts/Enemy/Neutrophil/NeutrophilShoot.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class NeutrophilShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+
+    [SerializeField] private float fireInterval = 0.2f;
+    private float timer = 0f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (/*controls.Player.Shoot.IsPressed() && これもMacrophageに必要なし*/
+            timer >= fireInterval)
+        {
+            timer = 0f;
+
+            Instantiate(
+                bulletPrefab,
+                firePoint.position,
+                Quaternion.identity
+            );
+        }
+    }
+}
