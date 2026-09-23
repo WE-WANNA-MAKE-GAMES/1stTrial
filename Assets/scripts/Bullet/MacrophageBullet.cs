e2e335db541f1f68dd7f3ec836c72e95ab895a41
Photeinos-Kochlias <mogurabu.j@gmail.com>
2026-09-23 18:47:24 +0900

    fix: マクロファージの弾の速さ修正

--- Assets/scripts/Bullet/MacrophageBullet.cs
+++ Assets/scripts/Bullet/MacrophageBullet.cs
@@ -17,9 +17,7 @@ public class MacrophageBullet : MonoBehaviour
 
     void Update()
     {
-        // Aim is in world space. ScrollRoot is a camera child, so adding that
-        // vector to localPosition also inherits scroll and makes rightward shots faster.
-        transform.localPosition += (Vector3)(direction * speed * Time.deltaTime);
+        transform.position += (Vector3)(direction * speed * Time.deltaTime);
 
         if (Camera.main != null &&
             Mathf.Abs(transform.position.x - Camera.main.transform.position.x) > destroyDistance)
